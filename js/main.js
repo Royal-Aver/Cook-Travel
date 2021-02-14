1f3c9c386063972720bbf49f8db843048d031fa1
Royal_Aver <royal_aver@rambler.ru>
2021-02-14 20:56:22 +0300

    report

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('.report__slider').slick({
+		dots:true,
+		adaptiveHeight:true,
+		slidesToShow:3,
+		slidesToScroll:2,
+		speed:1000,
+		autoplay:true,
+		autoplaySpeed:2500,
+		centerMode:true,
+
+	});
+});
+
